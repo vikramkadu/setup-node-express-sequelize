611520750dcd1cdfa982082cee490d6d7bca8510
Vikram Kadu <93420137+vikramkadu@users.noreply.github.com>
2024-02-12 15:52:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var cors = require("cors");
 const router = require("./routes");
 const auth = require("./middleware/auth");
 app.use(cors({
-    origin: ['http://localhost:4200',,'http://localhost:5000','https://efs-ui-dev.azurewebsites.net','https://efs.incubxperts.com']}));
+    origin: ['http://localhost:4200',,'http://localhost:5000']}));
 app.options(cors());
 
 app.use(express.json()); //parse the json data and put it into req.body
